ec03ab3cc188971be006d66c0ea1321b624bb003
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2025-09-27 13:49:28 +0530

    Time: 7 ms (41.03%) | Memory: 17.9 MB (39.84%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -11,11 +11,9 @@ class Solution:
             "M" : 1000
         }
         result = 0 #declaring the result variable with 0 
-        for i in range(len(s)-1): #so that the loop doesn't go overbound for the (i+1)element checking
-            if roman_map[s[i]] < roman_map[s[i+1]]: #if the next element is greater, than subtract IV-4,V-5
+        for i in range(len(s)): #so that the loop doesn't go overbound for the (i+1)element checking
+            if  i+1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]: #if the next element is greater, than subtract IV-4,V-5, also check the bouding statement, i+1 < len(s), so that the loop never goes unbounded 
                 result -= roman_map[s[i]] #subtracting into the result
             else:
                 result += roman_map[s[i]]
-        #for adding the last element, coz after the len(n)-1 comparison, last element not getting added , so we need to add it explictly
-        result += roman_map[s[-1]]
         return result
