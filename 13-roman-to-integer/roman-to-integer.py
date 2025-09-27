54213b9c4a43c0e26e1652aaef8584ed6c77215c
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2025-09-27 13:51:59 +0530

    Time: 7 ms (41.03%) | Memory: 17.9 MB (39.84%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,19 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-
-        roman_map={ #creating an hash map to add the value wrt alphabets
-            "I" : 1,
-            "V" : 5,
-            "X" : 10,
-            "L" : 50,
-            "C" : 100,
-            "D" : 500,
-            "M" : 1000
+        roman_map={ 
+            "I" : 1,"V" : 5,"X" : 10,"L" : 50,"C" : 100,"D" : 500,"M" : 1000
         }
-        result = 0 #declaring the result variable with 0 
-        for i in range(len(s)): #so that the loop doesn't go overbound for the (i+1)element checking
-            if  i+1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]: #if the next element is greater, than subtract IV-4,V-5, also check the bouding statement, i+1 < len(s), so that the loop never goes unbounded 
-                result -= roman_map[s[i]] #subtracting into the result
+        result = 0 
+        for i in range(len(s)):
+            if  i+1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]:
+                result -= roman_map[s[i]] 
             else:
                 result += roman_map[s[i]]
         return result
