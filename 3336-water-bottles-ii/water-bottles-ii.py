8f040aebe26749df354d7700a646738558283b1f
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2025-10-02 18:19:40 +0530

    Time: 33 ms (90.56%) | Memory: 18 MB (5.56%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.py
+++ 3336-water-bottles-ii/water-bottles-ii.py
@@ -3,14 +3,9 @@ class Solution:
         drink = 0
         empty = 0
         while numBottles > 0:
-            #drink all bottles
-
             drink +=numBottles
             empty +=numBottles
             numBottles = 0
-            
-            #try to exchange
-
             if empty >=numExchange:
                 empty -=numExchange
                 numBottles +=1
