3f0427071c428daa0ef6499b34f9e548e9ade4f9
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2025-10-09 12:38:40 +0530

    Time: 4 ms (15.28%) | Memory: 17.8 MB (61.52%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -4,11 +4,7 @@ class Solution:
         while n > 1:
             if n in seen:
                 return False
-            seen.add(n)
-            sq_sum = 0
-            while n > 0:
-                d = n%10
-                sq_sum += d * d
-                n //=10
-            n = sq_sum
+            else:
+                seen.add(n)
+                n = sum(int(d)**2 for d in str(n))  #list comprehension
         return True
