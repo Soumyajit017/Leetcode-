128f9be53102b7c9b7b75cc156c2583d0ab986e4
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2025-10-12 21:19:28 +0530

    Time: 6 ms (75.81%) | Memory: 17.8 MB (43.95%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,5 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n > 0:
-            return 1162261467 % n == 0 #1162261467 -> largest power of 3 within the given range, so if n is a power of 3, it will always divide 1162261467...
+            x = math.log10(n) / math.log10(3)
+            if float(int(x)) == x:
+                return True
         return False
