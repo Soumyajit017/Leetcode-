01ad85bbcd097fe6d6c88c029607ccc71b8a1e18
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2025-10-15 18:17:59 +0530

    Time: 7 ms (11.91%) | Memory: 17.6 MB (96.17%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,9 +1,7 @@
 from math import sqrt
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        if num == 1:
-            return True 
-        for i in range(2,int(sqrt(num))+1):
+        for i in range(1,int(sqrt(num))+1):
             if i == num/i:
                 return True
         return False
