6f3e29a9fde182cdeae18ce29c184d7addf03162
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2025-10-20 12:11:48 +0530

    Time: 3 ms (79.83%) | Memory: 17.9 MB (39.78%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,12 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_map={ 
-            "I" : 1,"V" : 5,"X" : 10,"L" : 50,"C" : 100,"D" : 500,"M" : 1000
-        }
-        result = 0 
-        for i in range(len(s)):
-            if  i+1 < len(s) and roman_map[s[i]] < roman_map[s[i+1]]:
-                result -= roman_map[s[i]] 
+        roman = {"I":1,"V":5, "X": 10, "L":50, "C":100, "D":500, "M":1000}
+        num = 0
+        n = len(s)
+        for i in range(n-1):
+            if roman[s[i]] < roman[s[i+1]] :
+                num -= roman[s[i]]
             else:
-                result += roman_map[s[i]]
-        return result
+                num += roman[s[i]]
+        num += roman[s[n-1]]
+        return num
