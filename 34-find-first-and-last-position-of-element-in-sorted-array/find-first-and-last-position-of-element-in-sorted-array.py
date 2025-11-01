84c02ab03162426b1a2a3eca27170144083c5018
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2025-11-01 13:35:29 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (75.08%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        l,r = 0, len(nums)-1
+        if not nums:
+            return [-1,-1]
+        res = []
+        while l <=r:
+            mid = l + (r-l)//2
+            if nums[mid]>=target:
+                r = mid-1
+            else:
+                l = mid+1 #returns the lower bound, also includes the insert position where it can be inserted if not present
+        if l < len(nums) and nums[l] == target: #for the safety, if the l doesn't exist why need to go for the right part
+            res.append(l)
+        else:
+            return [-1,-1]
+        l,r = 0,len(nums)-1
+        while l<=r:
+            mid = l + (r-l)//2
+            if nums[mid] <= target:
+                l = mid+1
+            else:
+                r = mid-1
+        res.append(r) 
+        return res
