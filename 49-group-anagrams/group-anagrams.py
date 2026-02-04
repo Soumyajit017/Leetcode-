6e968f8712828503aceaa993b39c4f8e4f577181
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2026-02-04 11:07:12 +0530

    Time: 18 ms (33.16%) | Memory: 24.2 MB (7.24%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = {}
+        dic = defaultdict(list) #skips the edge case if key not there
         for s in strs:
-            key = ''.join(sorted(s))
-            if key in groups:
-                groups[key].append(s)
-            else:
-                groups[key] = [s]
-        final = groups.values()
-        return list(final)
+            count = [0] * 26 #creates an array of 26 zeros
+            for ch in s:  #now go for individual elements
+                count[ord(ch)-ord('a')] += 1  #for 'eat', e-> count[4] = 1,then goes for all
+            key = tuple(count) #coz list ain't be dic keys
+            dic[key].append(s)  #appending to the final dics to return
+        return list(dic.values()) 
+
