e3973ac0dd8141ce29061ec7ad7e44475f172e12
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2026-02-05 15:15:40 +0530

    Time: 1340 ms (11.29%) | Memory: 22.1 MB (52.48%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,27 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        total = 0
-        result = []
-        n = len(nums)
-        if n < 3: 
-            return []
         nums.sort()
-        for i in range(0,n-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue 
-            left,right=i+1,n-1
+        sum = 0
+        hash_set = set()
+        for i in range(len(nums)-2):
+            if nums[i] > 0: break
+            right = len(nums)-1
+            left = i+1
             while left < right:
-                total = nums[i]+nums[left]+nums[right]
-                if total == 0:
-                    result.append([nums[i],nums[left],nums[right]])
-                    while left < right and nums[left] == nums[left+1]:
-                        left +=1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -=1
-                    left +=1
-                    right -=1
-                elif total < 0:
-                    left += 1 
+                sum = nums[i] + nums[left] + nums[right]
+                if sum == 0:
+                    hash_set.add((nums[i],nums[left],nums[right]))
+                    left+=1
+                    right-=1
+                elif sum < 0:
+                    left+=1
                 else:
-                    right -=1 
-        return result
+                    right-=1
+        return list(hash_set)
