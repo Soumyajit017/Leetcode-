306eac2805adfa4442206558c89df93fa428ec15
soumyajit chattaraj <138457832+Soumyajit017@users.noreply.github.com>
2026-02-06 12:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (51.67%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -6,13 +6,13 @@ class Solution:
             if nums[mid] == target:
                 return mid
             elif nums[l] <= nums[mid]:
-                if nums[l]<= target <nums[mid]:
-                    r = mid -1
+                if nums[l]<= target <= nums[mid]:
+                    r = mid - 1
                 else:
                     l = mid + 1
             else:
-                if nums[mid] < target <= nums[r]:
-                    l = mid+1
+                if nums[mid]< target <= nums[r]:
+                    l = mid + 1
                 else:
-                    r = mid -1
+                    r = mid - 1 
         return -1
